e8b8d821439eb185fea1f1de58837adf2a12f7f8
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2016-02-26 18:51:18 +0900

    + zh_CN i18n

---
+++ src/extensions/locale-zh_CN.php
@@ -0,0 +1,11 @@
+<?php
+final class Locale_zh_CN extends PhutilLocale {
+    public function getLocaleCode() {
+        return 'zh_CN';
+    }
+
+    public function getLocaleName() {
+        return '中华语言博大精深';
+    }
+}
+
